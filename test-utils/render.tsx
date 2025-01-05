4f111c58609fdceb6f4ea4402098f73e12c07cfb
Michael Sprague <mikesprague@gmail.com>
2025-01-04 19:40:03 -0500

    style(test-utils/render): use arrow function

--- test-utils/render.tsx
+++ test-utils/render.tsx
@@ -2,10 +2,10 @@ import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../src/theme';
 
-export function render(ui: React.ReactNode) {
+export const render = (ui: React.ReactNode) => {
   return testingLibraryRender(ui, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
       <MantineProvider theme={theme}>{children}</MantineProvider>
     ),
   });
-}
+};
